34ece25d0427b2defc415504b337004a2e4202e9
root <root@pageperso.novalocal>
2021-01-10 14:34:24 +0000

    save work

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+import requests
+
+app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/pages/views/<keywords>/<start>/<end>')
+@cross_origin()
+def fetch_wikipedia_pageviews_metric(keywords, start, end):
+    url = f"""https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/fr.wikipedia/all-access/user/{keywords}/daily/{start}/{end}"""
+    return requests.get(url).json()
--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -10,17 +10,18 @@ export class WikipediaService {
   constructor(private http: HttpClient) { }
 
   findMatchingArticles(keywords: string): Observable<any> {
-    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&search=${keywords}&limit=10&namespace=0&profile=engine_autoselect&redirects=resolve&exchars=1000&origin=*&format=json`;
+    const url = `https://fr.wikipedia.org/w/api.php?action=opensearch&search=${keywords}&limit=10&namespace=0&profile=engine_autoselect&redirects=resolve&exchars=1000&origin=*&format=json`;
     return this.http.get(url);
   }
 
   getStatisticsKeywords(matchingKeywords: string): Observable<any> {
-    const url = `https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/user/${matchingKeywords}/daily/2016122700/2020122600`;
+    //const url = `https://en.wikipedia.org/w/rest.php/v1/pageviews/per-article/en.wikipedia/all-access/user/${matchingKeywords}/daily/2016122700/2020122600`;
+    const url = `https://api.spiral.jonathanpastor.fr/pages/views/${matchingKeywords}/2016122700/2020122600`;
     return this.http.get(url);
   }
 
   getSummary(matchingKeywords: string): Observable<any> {
-    const url = `https://en.wikipedia.org/api/rest_v1/page/summary/${matchingKeywords}`;
+    const url = `https://fr.wikipedia.org/api/rest_v1/page/summary/${matchingKeywords}`;
     return this.http.get(url);
   }
 
